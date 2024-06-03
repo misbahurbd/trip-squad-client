1cd3d4a9a9c1b3824378fb10cbce506ae52ebc67
Misbahur Rahman <misbahurbd@gmail.com>
2024-06-04 05:50:48 +0600

    update privacy policy page and trems and conditions

--- app/(root)/terms-and-conditions/page.tsx
+++ app/(root)/terms-and-conditions/page.tsx
@@ -8,119 +8,129 @@ export const metadata: Metadata = {
 
 const TermsAndConditions = () => {
   return (
-    <div className="container p-6 rounded-lg bg-background">
-      <div className="mb-6">
-        <h1 className="text-3xl font-bold">Terms and Conditions</h1>
-        <p className="text-lg text-muted-foreground">
-          Effective Date: May 29, 2024
+    <div className="container">
+      <div className="p-6 rounded-lg bg-background">
+        <div className="mb-6">
+          <h1 className="text-3xl font-bold">Terms and Conditions</h1>
+          <p className="text-lg text-muted-foreground">
+            Effective Date: May 29, 2024
+          </p>
+        </div>
+        <p className="mb-4">
+          Welcome to Trip Squad. Please read these terms and conditions
+          carefully before using our service.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">1. Acceptance of Terms</h2>
+        <p className="mb-4">
+          By accessing or using the Trip Squad application, you agree to be
+          bound by these terms and conditions. If you do not agree with any part
+          of the terms, you may not use our service.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">2. Changes to Terms</h2>
+        <p className="mb-4">
+          We reserve the right to modify or replace these terms at any time.
+          Your continued use of the service following the posting of any changes
+          constitutes acceptance of those changes.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">
+          3. User Responsibilities
+        </h2>
+        <p className="mb-4">
+          As a user, you agree to use the service only for lawful purposes and
+          in a way that does not infringe the rights of, restrict, or inhibit
+          anyone else&apos;s use and enjoyment of the service. Prohibited
+          behavior includes harassing or causing distress or inconvenience to
+          any other user, transmitting obscene or offensive content, or
+          disrupting the normal flow of dialogue within our service.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">4. Account Registration</h2>
+        <p className="mb-4">
+          To use certain features of our service, you may be required to
+          register an account. You agree to provide accurate and complete
+          information and to keep this information up to date. You are
+          responsible for maintaining the confidentiality of your account
+          password and for all activities that occur under your account.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">5. Privacy Policy</h2>
+        <p className="mb-4">
+          Our Privacy Policy explains how we collect, use, and protect your
+          personal information. By using our service, you consent to the
+          collection and use of information in accordance with our Privacy
+          Policy.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">
+          6. Intellectual Property
+        </h2>
+        <p className="mb-4">
+          The service and its original content, features, and functionality are
+          and will remain the exclusive property of Trip Squad and its
+          licensors. Our trademarks and trade dress may not be used in
+          connection with any product or service without the prior written
+          consent of Trip Squad.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">7. Termination</h2>
+        <p className="mb-4">
+          We may terminate or suspend your account and bar access to the service
+          immediately, without prior notice or liability, under our sole
+          discretion, for any reason whatsoever and without limitation,
+          including but not limited to a breach of the terms.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">
+          8. Limitation of Liability
+        </h2>
+        <p className="mb-4">
+          In no event shall Trip Squad, nor its directors, employees, partners,
+          agents, suppliers, or affiliates, be liable for any indirect,
+          incidental, special, consequential, or punitive damages, including
+          without limitation, loss of profits, data, use, goodwill, or other
+          intangible losses, resulting from (i) your use or inability to use the
+          service; (ii) any unauthorized access to or use of our servers and/or
+          any personal information stored therein; (iii) any interruption or
+          cessation of transmission to or from the service; (iv) any bugs,
+          viruses, trojan horses, or the like that may be transmitted to or
+          through our service by any third party; (v) any errors or omissions in
+          any content or for any loss or damage incurred as a result of your use
+          of any content posted, emailed, transmitted, or otherwise made
+          available through the service; and/or (vi) the defamatory, offensive,
+          or illegal conduct of any third party. In no event shall Trip Squad be
+          liable for any claim, including but not limited to, your use of the
+          service, exceeding the amount paid by you to Trip Squad for the
+          service.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">9. Governing Law</h2>
+        <p className="mb-4">
+          These terms shall be governed and construed in accordance with the
+          laws of the United States, without regard to its conflict of law
+          provisions.
+        </p>
+
+        <h2 className="text-2xl font-semibold mb-2">10. Contact Us</h2>
+        <p className="mb-4">
+          If you have any questions about these Terms and Conditions, please
+          contact us at:
+        </p>
+        <p className="mb-4">
+          <strong>Email:</strong>{" "}
+          <a
+            href="mailto:support@tripsquad.com"
+            className="text-blue-600 hover:underline"
+          >
+            support@tripsquad.com
+          </a>
+        </p>
+        <p className="mb-4">
+          <strong>Address:</strong> 12/A Raynagar, Sylhet-3100, Bangladesh
         </p>
       </div>
-      <p className="mb-4">
-        Welcome to Trip Squad. Please read these terms and conditions carefully
-        before using our service.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">1. Acceptance of Terms</h2>
-      <p className="mb-4">
-        By accessing or using the Trip Squad application, you agree to be bound
-        by these terms and conditions. If you do not agree with any part of the
-        terms, you may not use our service.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">2. Changes to Terms</h2>
-      <p className="mb-4">
-        We reserve the right to modify or replace these terms at any time. Your
-        continued use of the service following the posting of any changes
-        constitutes acceptance of those changes.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">3. User Responsibilities</h2>
-      <p className="mb-4">
-        As a user, you agree to use the service only for lawful purposes and in
-        a way that does not infringe the rights of, restrict, or inhibit anyone
-        else&apos;s use and enjoyment of the service. Prohibited behavior
-        includes harassing or causing distress or inconvenience to any other
-        user, transmitting obscene or offensive content, or disrupting the
-        normal flow of dialogue within our service.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">4. Account Registration</h2>
-      <p className="mb-4">
-        To use certain features of our service, you may be required to register
-        an account. You agree to provide accurate and complete information and
-        to keep this information up to date. You are responsible for maintaining
-        the confidentiality of your account password and for all activities that
-        occur under your account.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">5. Privacy Policy</h2>
-      <p className="mb-4">
-        Our Privacy Policy explains how we collect, use, and protect your
-        personal information. By using our service, you consent to the
-        collection and use of information in accordance with our Privacy Policy.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">6. Intellectual Property</h2>
-      <p className="mb-4">
-        The service and its original content, features, and functionality are
-        and will remain the exclusive property of Trip Squad and its licensors.
-        Our trademarks and trade dress may not be used in connection with any
-        product or service without the prior written consent of Trip Squad.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">7. Termination</h2>
-      <p className="mb-4">
-        We may terminate or suspend your account and bar access to the service
-        immediately, without prior notice or liability, under our sole
-        discretion, for any reason whatsoever and without limitation, including
-        but not limited to a breach of the terms.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">
-        8. Limitation of Liability
-      </h2>
-      <p className="mb-4">
-        In no event shall Trip Squad, nor its directors, employees, partners,
-        agents, suppliers, or affiliates, be liable for any indirect,
-        incidental, special, consequential, or punitive damages, including
-        without limitation, loss of profits, data, use, goodwill, or other
-        intangible losses, resulting from (i) your use or inability to use the
-        service; (ii) any unauthorized access to or use of our servers and/or
-        any personal information stored therein; (iii) any interruption or
-        cessation of transmission to or from the service; (iv) any bugs,
-        viruses, trojan horses, or the like that may be transmitted to or
-        through our service by any third party; (v) any errors or omissions in
-        any content or for any loss or damage incurred as a result of your use
-        of any content posted, emailed, transmitted, or otherwise made available
-        through the service; and/or (vi) the defamatory, offensive, or illegal
-        conduct of any third party. In no event shall Trip Squad be liable for
-        any claim, including but not limited to, your use of the service,
-        exceeding the amount paid by you to Trip Squad for the service.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">9. Governing Law</h2>
-      <p className="mb-4">
-        These terms shall be governed and construed in accordance with the laws
-        of the United States, without regard to its conflict of law provisions.
-      </p>
-
-      <h2 className="text-2xl font-semibold mb-2">10. Contact Us</h2>
-      <p className="mb-4">
-        If you have any questions about these Terms and Conditions, please
-        contact us at:
-      </p>
-      <p className="mb-4">
-        <strong>Email:</strong>{" "}
-        <a
-          href="mailto:support@tripsquad.com"
-          className="text-blue-600 hover:underline"
-        >
-          support@tripsquad.com
-        </a>
-      </p>
-      <p className="mb-4">
-        <strong>Address:</strong> 12/A Raynagar, Sylhet-3100, Bangladesh
-      </p>
     </div>
   )
 }
