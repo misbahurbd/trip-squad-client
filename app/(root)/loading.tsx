291b153c79026d491b75c7d069de1eaee92396ff
Misbahur Rahman <misbahurbd@gmail.com>
2024-06-03 20:35:58 +0600

    update loading component

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -1,10 +1,8 @@
-import { ScaleLoader } from "react-spinners"
-
 const Loading = () => {
   return (
-    <div className="w-full min-h-[50vh] flex-1 flex items-center justify-center flex-col gap-3 bg-secondary">
-      <ScaleLoader className="size-10" />
-      <p className="text-sm text-center text-muted-foreground">Loading...</p>
+    <div className="w-full min-h-[50vh] flex-1 flex items-center justify-center flex-col gap-5 bg-secondary">
+      <div className="size-8 bg-muted-foreground rounded-sm animate-spin" />
+      <p className=" text-center text-muted-foreground">Loading...</p>
     </div>
   )
 }
--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -2,9 +2,9 @@ import { ScaleLoader } from "react-spinners"
 
 const Loading = () => {
   return (
-    <div className="w-full flex-1 min-h-[50vh] flex items-center justify-center flex-col gap-3 bg-secondary">
-      <ScaleLoader className="size-10" />
-      <p className="text-sm text-center text-muted-foreground">Loading...</p>
+    <div className="w-full flex-1 min-h-[50vh] flex items-center justify-center flex-col gap-5 bg-secondary">
+      <div className="size-8 bg-muted-foreground rounded-sm animate-spin" />
+      <p className=" text-center text-muted-foreground">Loading...</p>
     </div>
   )
 }
