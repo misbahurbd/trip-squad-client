fde230905d40087070160d62e3d4941b796a8d94
Misbahur Rahman <misbahurbd@gmail.com>
2024-07-16 23:40:07 +0600

    add user demo account credentials

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import LoginForm from "@/components/form/login-form"
 import { Suspense } from "react"
 import authImg from "@/assets/img/auth-img-01.jpg"
+import { Separator } from "@/components/ui/separator"
 
 export const metadata: Metadata = {
   title: "Login | Trip Squad",
@@ -24,6 +25,17 @@ const LoginPage = () => {
       </div>
       <div className="md:w-1/2 bg-background rounded-xl py-8 md:py-12 px-8 md:px-10 flex flex-col items-center gap-8">
         <h2 className="text-2xl font-bold text-center">Login Your Account</h2>
+        <Separator />
+        <div className="">
+          <h4>Admin Credentials</h4>
+          <p>
+            <span>Username:</span> misbahurbd
+          </p>
+          <p>
+            <span>Password:</span> Pass@000
+          </p>
+        </div>
+        <Separator />
         <Suspense>
           <LoginForm />
         </Suspense>
